8a9162815c9fb08d59bec92b0f5f68bdde5573b8
Aashir Ahmed <aashir.waqar@arbisoft.com>
2023-08-27 17:47:20 +0500

    added code to send-emails

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from templated_mail.mail import BaseEmailMessage
+from django.core.mail import BadHeaderError
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+        message = BaseEmailMessage(
+            template_name="emails/hello.html", context={"name": "Aashir"}
+        )
+        message.send(["john@gmail.com"])
+    except BadHeaderError:
+        pass
+    return render(request, "hello.html", {"name": "Mosh"})
